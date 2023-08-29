b300080f9b746d9e713c8004fb39804b3f0fb5cf
harshit jharotiya <jharotiyaharshit@gmail.com>
2023-08-29 18:17:55 +0530

    mern app

--- new.js
+++ new.js
@@ -22,7 +22,9 @@ const userRouter = require("./Routers/userR");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
+  await mongoose.connect(
+    "mongodb+srv://jharotiyaharshit:kfSQP1BVaSSwltmU@cluster0.g5sgp61.mongodb.net/test?retryWrites=true&w=majority"
+  );
   console.log("database connected");
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
